ba303c6ede4e6e985827a08d1fbea3bd20955000
r4deadv <mihovilovic.rade@gmail.com>
2022-08-27 12:35:29 +0200

    creating a store in index.js file

--- src/index.js
+++ src/index.js
@@ -2,6 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./components/App";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import reducer from "./reducers";
+
+const store = createStore(reducer);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
